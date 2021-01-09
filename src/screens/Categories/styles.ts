0fd07cefd8de6743bf5318d8bd2e6438d01860af
ozorku <adavizeozorku@gmail.com>
2021-01-09 19:01:22 +0100

    add caregories screen

--- src/screens/Categories/styles.ts
+++ src/screens/Categories/styles.ts
@@ -2,7 +2,13 @@ import { StyleSheet, Platform } from "react-native";
 import { colors, getHeight, getWidth } from "../../styles";
 
 const styles = () => {
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    grid: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      flexWrap: "wrap",
+    },
+  });
 };
 
 export default styles;
